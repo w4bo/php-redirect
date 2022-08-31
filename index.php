d469c4da02f3f0cd6f922fb6bf04ca6597e878ef
Matteo Francia <PERSONALE\matteo.francia5@isi-big-web.campusfc.dir.unibo.it>
2022-08-31 13:39:19 +0200

    update redirect

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
-
 <?php
-$url = $_POST['url'];
-$data = $_POST['data'];
-$method = $_POST['method'];
-$header = $_POST['header'];
-$options = array(
-    'http' => array(
-        'header'  => $header,
-        'method'  => $method,
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-var_dump($result);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+
+$url = $_GET["url"];
+$params = "?" . $_SERVER["QUERY_STRING"];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url . $params);  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Set request method to POST
+    curl_setopt($ch, CURLOPT_POST, 1);
+    // Set query data here with CURLOPT_POSTFIELDS
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+}
+$content = trim(curl_exec($ch));
+curl_close($ch);
+print $content;
 ?>
